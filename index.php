acdcb2a1f4584db4225c12bad207820b5fbe02e2
Yusups20 <yups2092003.com>
2019-09-24 11:46:33 +0700

    bootstrap

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <title>Latihan Bootstrap</title>
+    <!-- Pemanggilan css -->
+    <link rel="stylesheet" href="/assets/css/bootstrap.min.css">
+    <!-- Akhir Pemanggilan css -->
+</head>
+
+<body>
+    <!-- Header -->
+    <!-- End Header -->
+
+    <!-- Content -->
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12" style="background:black">
+                <h1 style="color:white" align="center">12 Coloum</h1>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3" style="background:red">
+                <h1 style="color:white">6 Coloum</h1>
+            </div>
+            <div class="col-md-3" style="background:green">
+                <h1 style="color:white">6 Coloum</h1>
+            </div>
+            <div class="col-md-3" style="background:yellow">
+                <h1 style="color:white">6 Coloum</h1>
+            </div>
+            <div class="col-md-3" style="background:blue">
+                <h1 style="color:white">6 Coloum</h1>
+            </div>
+        </div>
+    </div>
+    <!-- End Content -->
+
+    <!-- Footer -->    
+    <!-- End Footer -->
+
+    <!-- Pemanggilan JS -->
+    <script src="/assets/js/bootstrap.bundle.js"></script>
+    <script src="/assets/js/bootstrap.bundle.min.js"></script>
+    <!-- Akhir PEmangiilan JS -->
+</body>
+</html>
